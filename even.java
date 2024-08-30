5575b1fa18aa832e1f745ae624f8002270dbaef9
hardikvaskle <hardikvaskle30@gmail.com>
2024-08-30 09:51:39 +0530

    print even number

---
+++ even.java
@@ -0,0 +1,14 @@
+
+
+public class even {
+    public static void main(String[] args) {
+       
+        int a =40 ;
+        for(int i = 0; i<=a; i++){
+            if(i%2==0){
+                System.out.println(i);
+                
+            }
+        }
+    }
+}
