5e8e70b9bf172a15ae85cabb77102bb22b6b95cc
hardikvaskle <hardikvaskle30@gmail.com>
2024-09-07 14:45:05 +0530

    using string

---
+++ string.java
@@ -0,0 +1,15 @@
+public class string {
+    public static void main(String[] args) {
+       
+        String name = "hardik";
+        int value = name.length();
+        System.out.println(value);
+
+        //also use below code 
+
+        String name = new String("hardik");
+        int value = name.length();
+        System.out.println(value);
+    }
+    
+}
