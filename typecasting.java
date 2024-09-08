9416ae8e58f9ef23905679a6957ef1c76b474bb6
hardikvaskle <hardikvaskle30@gmail.com>
2024-09-08 20:35:14 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,24 @@
+public class typecasting {
+    public static void main(String[] args) {
+       
+      /*implicit 
+          int num = 10;
+          System.out.println("The integer value: " + num);
+        
+          double data = num;
+          System.out.println("The double value: " + data);*/
+
+      //explicit 
+                
+          double num = 10.99;
+          System.out.println("The double value: " + num);
+                
+          int data = (int) num;
+          System.out.println("The integer value: " + data);
+                  
+        
+
+        
+    }
+    
+}
