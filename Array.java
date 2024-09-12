a26762fea8879cd28fbd2aaca3fac7ef90220c79
hardikvaskle <hardikvaskle30@gmail.com>
2024-09-12 23:54:34 +0530

    array1

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+
+        int n = 8;
+        int arr[] = {1,2,3,4,5,6,7,8};
+
+        for (int i = 0; i<n; i++){
+            System.out.println("value at index"+i+":- "+arr[i]);
+        }
+    }
+}
