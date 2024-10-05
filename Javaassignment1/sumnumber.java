e9a98dd1e209c07aa01f7f8a6372a30bfdc09b19
hardikvaskle <hardikvaskle30@gmail.com>
2024-10-05 19:34:06 +0530

    sum of natural number

---
+++ Javaassignment1/sumnumber.java
@@ -0,0 +1,14 @@
+//sum of natural number upto n
+public class sumnumber {
+    public static void main(String[] args) {
+        int i = 1;
+        int n = 10;
+         int sum = 0;
+
+         for( i = 1; i<=n; i++ ){
+            sum = sum + i;
+         }
+
+         System.out.println(sum);
+    }
+}
