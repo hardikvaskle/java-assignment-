c504fd04fdfda896fc825d444eee45fce31422bc
hardikvaskle <hardikvaskle30@gmail.com>
2024-10-05 19:23:36 +0530

    reverse a number

---
+++ Javaassignment1/reversenumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class reversenumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int number = scanner.nextInt(); 
+        int reversedNumber = 0;
+        
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        
+        System.out.println( reversedNumber);
+        scanner.close();
+    
+    }
+}
